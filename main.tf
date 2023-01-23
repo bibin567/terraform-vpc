905c96f9a679bf0fd88f25b218a8151b80150b96
Bibin <88414576+bibin567@users.noreply.github.com>
2023-01-23 11:30:53 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+# main.tf
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "mysubnet1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = var.subnet1_cidr
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name = var.subnet1_name
+  }
+}
+
+resource "aws_subnet" "mysubnet2" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = var.subnet2_cidr
+  availability_zone = "eu-west-2b"
+  tags = {
+    Name = var.subnet2_name
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,24 @@
+# variables.tf
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet1_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "subnet2_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "vpc_name" {
+  default = "MyVpc"
+}
+
+variable "subnet1_name" {
+  default = "mysubnet1"
+}
+
+variable "subnet2_name" {
+  default = "mysubnet2"
+}
